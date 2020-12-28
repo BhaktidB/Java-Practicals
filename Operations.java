0a31088b2353e589838173d46a85b663bbefe587
BhaktidB <61202065+BhaktidB@users.noreply.github.com>
2020-12-28 23:29:14 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,32 @@
+/*experiment:4
+ * AIM: Program on Packages
+* 
+* NAME: BHAKTI DEEPAK BAILURKAR
+* ROLLNO: 1
+* DIV: A
+*/
+
+package operations;
+
+public class Operations {
+	
+	public static double add(double num1, double num2) {
+		return num1 + num2 ;
+		
+	}
+	
+	public static double sub(double num1, double num2) {
+		return num1 - num2 ;
+		
+	}
+	
+	public static double prod(double num1, double num2) {
+		return num1 * num2 ;
+		
+	}
+	
+	public static double div(double num1, double num2) {
+		return num1 / num2 ;
+		
+	}
+}
